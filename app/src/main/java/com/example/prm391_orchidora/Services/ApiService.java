35b363ddb314b495a6014df8b9ef9f36ebb207c3
kietpt2003 <tuankiet29012003@gmail.com>
2024-07-08 10:43:52 +0700

    fix api

--- app/src/main/java/com/example/prm391_orchidora/Services/ApiService.java
+++ app/src/main/java/com/example/prm391_orchidora/Services/ApiService.java
@@ -1,14 +1,12 @@
 package com.example.prm391_orchidora.Services;
 
-import android.util.Log;
-
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiService {
     private Retrofit retrofit;
-    private static final String BASE_URL = "http://10.0.2.2:8080/api/v1/";
+    private static final String BASE_URL = "http://52.196.189.219/api/v1/";
 
     public Retrofit getRetrofitInstance() {
         if(retrofit == null) {
